693119f3ecd5fbcd3af6a87ff0f33e71deab2973
AQuoc98 <badguykendf@gmail.com>
2023-04-10 13:56:07 +0700

    ghibli/feat/3d: use a custom toon shader

--- ghibli-style-threejs/src/Trees.jsx
+++ ghibli-style-threejs/src/Trees.jsx
@@ -1,10 +1,22 @@
-import React, { forwardRef, useMemo, useRef } from "react";
+import React, { forwardRef, useMemo } from "react";
 import { useGLTF } from "@react-three/drei";
-import { Color, DataTexture, RedFormat } from "three";
-import * as SimpleShader from "./SimpleShader";
+import { Color, Vector3 } from "three";
+import { ToonShader } from "./ToonShader";
 
 export const Trees = forwardRef((props, ref) => {
   const { nodes } = useGLTF("/trees.glb");
+
+  const uniforms = useMemo(() => {
+    return {
+      ...ToonShader.uniforms,
+      uBaseColor: { value: new Color("#49897C") },
+      uAmbientLightColor: { value: new Color("#050505") },
+      uDirLightColor: { value: new Color("white") },
+      uDirLightPos: { value: new Vector3(15, 15, 15) },
+      uLineColor1: { value: new Color("#808080") },
+      uLineColor2: { value: new Color("black") },
+    };
+  }, []);
 
   return (
     <group {...props} dispose={null} ref={ref}>
@@ -14,10 +26,7 @@ export const Trees = forwardRef((props, ref) => {
         geometry={nodes.Foliage.geometry}
         position={[0.33, -0.05, -0.68]}
       >
-        {/* <meshStandardMaterial
-          color={new Color("#33594e").convertLinearToSRGB()}
-        /> */}
-        <shaderMaterial attach="material" {...SimpleShader} />
+        <shaderMaterial attach="material" {...ToonShader} uniforms={uniforms} />
       </mesh>
     </group>
   );
